0bd05c7051dfa95ef81b7d6933f46674cdf0585b
Loïc Lescouzeres <llescouzeres@deloitte.fr>
2025-08-20 14:52:16 +0200

    FIX some s3 checkov alert

--- terraform/ut_data_s3.tf
+++ terraform/ut_data_s3.tf
@@ -15,6 +15,28 @@ resource "aws_s3_bucket_public_access_block" "ut_data_block" {
   restrict_public_buckets = true
 }
 
+resource "aws_s3_bucket_versioning" "ut_data_bucket" {
+  bucket = aws_s3_bucket.ut_data_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "ut_data_lifecycle_policy" {
+  bucket = aws_s3_bucket.ut_data_bucket.id
+  rule {
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
+
+    filter {
+      prefix = ""
+    }
+
+    id     = "remove-incomplete"
+    status = "Enabled"
+  }
+}
 
 resource "aws_s3_bucket_policy" "ut_data_bucket_policy" {
   bucket = aws_s3_bucket.ut_data_bucket.id
