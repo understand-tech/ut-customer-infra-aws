e8b5f5599b7db41b44831e76d302dfb75fc41141
Loïc Lescouzeres <llescouzeres@deloitte.fr>
2025-08-22 12:56:20 +0200

    Change mongodb port to string

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -4,7 +4,7 @@ locals {
   ut_redis_port              = 6379
   ut_llm_container_port      = 8000
   ut_llm_elb_port            = 80
-  ut_mongodb_container_port  = 27017
+  ut_mongodb_container_port  = "27017"
   ut_efs_port                = 2049
 
   # Use custom domain if provided, otherwise use CloudFront distribution domain
